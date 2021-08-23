0007ffcbd46f7f0f8716bb867514ae518947202d
Samuel Felipe <samuelfelipef@gmail.com>
2021-08-23 12:52:59 -0300

    refactoring

--- src/components/Stories/Stories.js
+++ src/components/Stories/Stories.js
@@ -1,41 +1,41 @@
 import { Story } from "./Story";
 
-const Stories = () => {
+const users = [
+    {
+        username: "9gag",
+        img: "./assets/img/9gag.svg"
+    },
+    {
+        username: "meowed",
+        img: "./assets/img/meowed.svg"
+    },
+    {
+        username: "barked",
+        img: "./assets/img/barked.svg"
+    },
+    {
+        username: "nathanwpylestrangeplanet",
+        img: "./assets/img/nathanwpylestrangeplanet.svg"
+    },
+    {
+        username: "wawawicomics",
+        img: "./assets/img/wawawicomics.svg"
+    },
+    {
+        username: "respondeai",
+        img: "./assets/img/respondeai.svg"
+    },
+    {
+        username: "filomoderna",
+        img: "./assets/img/filomoderna.svg"
+    },
+    {
+        username: "memeriagourmet",
+        img: "./assets/img/memeriagourmet.svg"
+    },
+];
 
-    const users = [
-        {
-            username: "9gag",
-            img: "./assets/img/9gag.svg"
-        },
-        {
-            username: "meowed",
-            img: "./assets/img/meowed.svg"
-        },
-        {
-            username: "barked",
-            img: "./assets/img/barked.svg"
-        },
-        {
-            username: "nathanwpylestrangeplanet",
-            img: "./assets/img/nathanwpylestrangeplanet.svg"
-        },
-        {
-            username: "wawawicomics",
-            img: "./assets/img/wawawicomics.svg"
-        },
-        {
-            username: "respondeai",
-            img: "./assets/img/respondeai.svg"
-        },
-        {
-            username: "filomoderna",
-            img: "./assets/img/filomoderna.svg"
-        },
-        {
-            username: "memeriagourmet",
-            img: "./assets/img/memeriagourmet.svg"
-        },
-    ];
+const Stories = () => {
 
     return (
 
