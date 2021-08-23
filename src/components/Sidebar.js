4fb32febf9c974b2785b24f995be300f116ed2cb
Samuel Felipe <samuelfelipef@gmail.com>
2021-08-23 18:51:19 -0300

    refactoring

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -14,6 +14,14 @@ const Sidebar = () => {
         <div class="sidebar">
             <User username={user.username} name={user.name} userImg={user.userImg} />
             <Suggestions />
+
+            <div class="links">
+                Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes • Hashtags • Idioma
+            </div>
+
+            <div class="copyright">
+                © 2021 INSTAGRAM DO FACEBOOK
+            </div>
         </div>
     )
 }
